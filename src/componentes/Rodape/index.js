bb96bf6144886f513a1a429300c80e0cd2c07c19
Vinicius <viniciusr.camargo@gmail.com>
2024-06-27 07:20:00 -0300

    Projeto criado em desenvolvimento de curso react web

---
+++ src/componentes/Rodape/index.js
@@ -0,0 +1,9 @@
+import './Rodape.css'
+
+const Rodape = () => {
+    return (<footer className='rodape'>
+        <p>© 2024 - Todos os direitos reservados a Vinicius Rodrigues Camargo</p>
+    </footer>)
+}
+
+export default Rodape
